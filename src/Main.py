b1a89f67c8f7bc60c43868d50ba8b80b329af532
slingthor <ingthorh@gmail.com>
2015-09-15 10:33:51 -0700

    test

---
+++ src/Main.py
@@ -0,0 +1,11 @@
+__author__ = 'ingthor'
+
+from src.Algorithms import MergeSort
+
+# create array
+# Algorithm function
+# perform algorithm
+
+#InsertionSort.Test(20000, InsertionSort.InsertionSort)
+#InsertionSort.Test(30, InsertionSort.InsertionSortAsc)
+MergeSort.Test(6)
